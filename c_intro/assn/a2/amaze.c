5e7a070017b32337952c8f7df5a2c66047db3181
Ian Price <michael.ian.price@gmail.com>
2017-05-05 17:51:32 -0400

    assignment 2 completed

--- c_intro/assn/a2/amaze.c
+++ c_intro/assn/a2/amaze.c
@@ -34,35 +34,85 @@ void amaze2() {
 void amaze3() {
 	int i = 1;
 	printf("  amaze3:\t");
-
+	again:
+	if (i % 2 == 1) {
+		printf("%d ",i);
+	}
+	i++;
+	if (i < 10) {
+		goto again;
+	}
 	printf("\n");
 }
 
 void amaze4() {
 	int i = 1;
 	printf("  amaze4:\t");
-
+	for (;;) {
+		if (i % 2 == 1) {
+			printf("%d ", i);
+		}
+		i++;
+		if (i >= 10) {
+			goto done;
+		}
+	}
+	done:
 	printf("\n");
 }
 
 void amaze5() {
 	int i = 1;
+	int j;
 	printf("  amaze5:\t");
-
+	check:
+	if (i >= 10) goto end;
+	j = i % 2;
+	if (j == 1) printf("%d ",i);
+	i++;
+	goto check;
+	end:
 	printf("\n");
 }
 
 void amaze6() {
 	int i = 1;
+	int j;
 	printf("  amaze6:\t");
-
+	start:
+	if (i >= 10) goto end;
+	j = i % 2;
+	switch (j) {
+		case 1:
+			printf("%d ",i);
+		default:
+			i++;
+			goto start;
+	}
+	end:
 	printf("\n");
 }
 
 void amaze7() {
 	int i = 1;
+	int count = 0;
 	printf("  amaze7:\t");
-
+	for (;;) {
+		if (i >= 10) goto end;
+		if (i % 2 == 1) {
+			goto odd;
+		} else {
+			goto even;
+		}
+		loop:
+		count++;
+	}
+	odd:
+	printf("%d ",i);
+	even:
+	i++;
+	goto loop;
+	end:
 	printf("\n");
 }
 
