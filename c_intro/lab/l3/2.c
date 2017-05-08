5bbe6887288602f32abfaa7db56c658825c2203b
Ian Price <michael.ian.price@gmail.com>
2017-05-08 13:49:03 -0400

    heap version lab3ex2 complete

--- c_intro/lab/l3/2.c
+++ c_intro/lab/l3/2.c
@@ -1,13 +1,26 @@
-#include <stdio.h>  
+#include <stdio.h>
 #include <string.h>
-int main() { 	
-	char str[15]; 	
+#include <stdlib.h>
+int main() {
+	char * str = (char *) malloc(3*sizeof(char));
+	strcpy(str, "hi");
+	printf("%s\n", str);
+	free(str);
+	char *str2 = str + 1;
+	printf("%s\n",str2);
+	strcpy(str2, "hello");
+	printf("%s\n", str2);
+	
+	
+	str = (char *) malloc(6*sizeof(char));
+	strcpy(str, "hello");
+	printf("%s\n", str);
+	printf("%s\n",str2);
 
-	strcpy(str, "hakuna matata!"); 
-	// this line should copy "hakuna matata!" 	                         
-	// into our char array 	
-	printf("%s\n", str); 	
-	// Anything else? 
+	free(str);
+	free(str2);
+	printf("%s\n", str);
+
 	return 0;
 }
 
